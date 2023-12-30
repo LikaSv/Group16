d566e897463d64acaf3e3491ed4ee111a289d7e7
LikaSv <45724202+LikaSv@users.noreply.github.com>
2023-12-30 20:42:16 +0400

    Add files via upload
    ავტვირთე ფაილი

---
+++ Classwork.py
@@ -0,0 +1,32 @@
+# import my_module
+# print(my_module.avg(3, 6))
+
+
+# import pkg.mod2
+# from pkg import mod2
+# from pkg.mod2 import jami
+# # print(pkg.mod2.jami(3, 6))
+# # print(mod2.jami(3, 6))
+# print(jami(3, 6))
+
+# import os
+# os.system('pip install deteyytu')
+from dateutil.easter import *
+print(easter(2024, 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
